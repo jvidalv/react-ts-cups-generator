1424a7dcefd6701bdd8df52fa7bc17abc9b645ae
Josep Vidal <josepvidalvidal@gmail.com>
2020-01-22 23:33:28 +0100

    Updates

---
+++ precache-manifest.8584c1d1fd9cacd3234a31783a323194.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "159564eb2afe4dca40f0418bc7914a6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2dc9f807d2c2362764e",
+    "url": "/static/css/main.7375a917.chunk.css"
+  },
+  {
+    "revision": "b0e1483adc49f8484bc0",
+    "url": "/static/js/2.8d2cc9f7.chunk.js"
+  },
+  {
+    "revision": "c6f2a8cff5bda75ce9d9d2e5dad3a2a7",
+    "url": "/static/js/2.8d2cc9f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2dc9f807d2c2362764e",
+    "url": "/static/js/main.0d682e2d.chunk.js"
+  },
+  {
+    "revision": "0a434f1b3745172bc1ce",
+    "url": "/static/js/runtime-main.a63f0d3a.js"
+  }
+]);
